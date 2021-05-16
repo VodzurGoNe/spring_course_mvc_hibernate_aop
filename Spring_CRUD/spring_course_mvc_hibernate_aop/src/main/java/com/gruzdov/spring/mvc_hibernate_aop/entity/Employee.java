34ab307a09b99476aabbdc78c109510317bf8974
Vladislav Gruzdov <gruzdov.v.1991@hotmail.com>
2021-05-16 19:07:00 +0400

    "Заготовка"

---
+++ Spring_CRUD/spring_course_mvc_hibernate_aop/src/main/java/com/gruzdov/spring/mvc_hibernate_aop/entity/Employee.java
@@ -0,0 +1,33 @@
+package com.gruzdov.spring.mvc_hibernate_aop.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "department")
+    private String department;
+
+    @Column(name = "salary")
+    private Integer salary;
+}
